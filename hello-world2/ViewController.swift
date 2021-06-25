7678e420103c7ff6524cab82947e25f83fa05b3e
kateprez <kate@prezioso.com.au>
2021-06-25 15:41:07 +1000

    initialise an iOS project

---
+++ hello-world2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hello-world2
+//
+//  Created by Kate Prezioso on 25/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
